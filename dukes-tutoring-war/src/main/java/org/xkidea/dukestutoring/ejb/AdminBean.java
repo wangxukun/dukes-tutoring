be13509e34554947aaf53eaaacc2cbbd04dcc96c
xk <wang_xukun@163.com>
2021-02-25 00:36:49 +0800

    h1

--- dukes-tutoring-war/src/main/java/org/xkidea/dukestutoring/ejb/AdminBean.java
+++ dukes-tutoring-war/src/main/java/org/xkidea/dukestutoring/ejb/AdminBean.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Path("/tutoring/admin")
+//@Path("/tutoring/admin")
 @Stateless
 @Named
 public class AdminBean {
--- dukes-tutoring-war/src/main/java/org/xkidea/dukestutoring/ejb/ConfigBean.java
+++ dukes-tutoring-war/src/main/java/org/xkidea/dukestutoring/ejb/ConfigBean.java
@@ -1,10 +1,19 @@
 package org.xkidea.dukestutoring.ejb;
 
+import org.xkidea.dukestutoring.entity.Administrator;
+import org.xkidea.dukestutoring.entity.Guardian;
+import org.xkidea.dukestutoring.entity.Student;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
 @Singleton
 @Startup
@@ -12,4 +21,100 @@ public class ConfigBean {
     @PersistenceContext
     private EntityManager em;
     private CriteriaBuilder cb;
+    @EJB
+    private AdminBean adminBean;
+    private static final Logger logger = Logger.getLogger("dukestutoring.ejb.ConfigBean");
+
+    // TODO ConfigBean
+
+    @PostConstruct
+    public void init() {
+        // create Maeby
+        logger.info("Creating Maeby entity");
+        Student maeby = new Student();
+        maeby.setLastName("Fünke");
+        maeby.setFirstName("Maeby");
+        maeby.setGrade(10);
+        maeby.setSchool("Sunshine Academy");
+
+        // create Maeby's dad
+        Guardian tobias = new Guardian();
+        tobias.setFirstName("Tobias");
+        tobias.setLastName("Fünke");
+        tobias.setEmail("tobias@example.com");
+        tobias.setPassword("javaee");
+
+        // create Maeby's mom
+        Guardian lindsey = new Guardian();
+        lindsey.setFirstName("Lindsey");
+        lindsey.setLastName("Fünke");
+        lindsey.setEmail("lindsey@example.com");
+        lindsey.setPassword("javaee");
+
+        // create George Michael
+        logger.info("Creating George Michael entity");
+        Student georgeMichael = new Student();
+        georgeMichael.setLastName("Bluth");
+        georgeMichael.setFirstName("George");
+        georgeMichael.setMiddleName("Michael");
+        georgeMichael.setGrade(10);
+        georgeMichael.setSchool("Huntington Beach High School");
+
+        // create GOB
+        logger.info("Creating Gob entity");
+        Student gob = new Student();
+        gob.setLastName("Bluth");
+        gob.setFirstName("George");
+        gob.setMiddleName("Oscar");
+        gob.setNickname("Gob");
+        gob.setGrade(12);
+        gob.setSchool("Magician's Alliance Institute");
+
+        // create Buster
+        logger.info("Creating Buster entity");
+        Student buster = new Student();
+        buster.setFirstName("Byron");
+        buster.setLastName("Bluth");
+        buster.setNickname("Buster");
+        buster.setGrade(11);
+        buster.setSchool("Milford Academy");
+
+        // create Lucille
+        logger.info("Creating Lucille entity");
+        Guardian lucille = new Guardian();
+        lucille.setFirstName("Lucille");
+        lucille.setLastName("Bluth");
+        lucille.setEmail("lucille@example.com");
+        lucille.setPassword("javaee");
+
+        logger.info("Calling createStudent() on Maeby");
+        String result = adminBean.createStudent(maeby);
+        logger.info("Calling createStudent() on George Michael");
+        result = adminBean.createStudent(georgeMichael);
+        logger.info("Calling createStudent() on Gob");
+        result = adminBean.createStudent(gob);
+
+        logger.info("Calling createGuardian() for Maeby's parents");
+        adminBean.createGuardian(tobias, maeby);
+        adminBean.createGuardian(lindsey, maeby);
+
+        logger.info("Calling createGuardian() for Buster and GOB's mom");
+        List<Student> lucilleKids = new ArrayList<>();
+        lucilleKids.add(gob);
+        lucilleKids.add(buster);
+        adminBean.createGuardianWithList(lucille, lucilleKids);
+
+//        logger.info("Checking in Maeby and George Michael");
+//        requestBean.checkIn(maeby);
+//        requestBean.checkIn(georgeMichael);
+
+        // create admin
+        Administrator admin = new Administrator();
+        admin.setFirstName("Admin");
+        admin.setLastName("Administrator");
+        admin.setEmail("admin@example.com");
+        admin.setPassword("javaee");
+        result = adminBean.createAdministrator(admin);
+
+    }
 }
